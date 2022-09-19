f94b345140a49450a7d7490b89a920b74eb03abe
CouchMeka <meka.daniels1@gmail.com>
2022-09-19 16:47:11 -0400

    stretch goals

--- stretch.js
+++ stretch.js
@@ -10,7 +10,7 @@ red = red.toLowerCase();
 let purple = "blue + red";
 let orange = "red + yellow";
 let green = "yellow + blue";
-let prompt1 = String(prompt("Pick a colors or two color combinations from the color wheel: "));
+let prompt1 = String(prompt("Pick a color or two color combinations from the color wheel: "));
 prompt1 = prompt1.toLowerCase();
 
 
@@ -21,16 +21,21 @@ if(prompt1.includes('blue') && prompt1.includes('red'))
 } else if(prompt1.includes('blue') && prompt1.includes('yellow'))
 {
     console.log("green");
+    
 } else if (prompt1.includes('yellow') && prompt1.includes('red'))
 {
     console.log("orange");
+
 } else if (prompt1 === "purple")
 {
     console.log(purple);
+
 } else if (prompt1 === 'orange')
 {
-    console.log(orange)
+    console.log(orange);
+
 } else if (prompt1 === 'green')
-  {
-    console.log(green)
-  } else {console.log("error")} 
+{
+    console.log(green);
+
+} else {console.log("error")} 
